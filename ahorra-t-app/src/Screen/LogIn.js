5550505cf7812164edba8e97dd1dc0d22aacfc81
FrankZV <135568329+FrankZV@users.noreply.github.com>
2023-06-13 20:09:44 -0600

    Login cambios

---
+++ ahorra-t-app/src/Screen/LogIn.js
@@ -0,0 +1,59 @@
+import {View, Text, StyleSheet} from 'react-native';
+import StyledText from '../components/StyledText'
+import { TouchableOpacity } from 'react-native';
+
+    
+const Longin = () => {
+    return(
+        <View style={styles.container} >
+             <Text style={{ textAlign: "center"}} fontWeight='italic' color='secondary'>Unete Ahorra.</Text>
+            <Text style={{ textAlign: "center"}} fontWeight='italic' color='secondary'>Crea una cuenta donde empezar a conocer{'\n'}sobre educación financiera{'\n'}además tendras acceso a una cuenta{'\n'}bancaria y otros beneficios!</Text>
+            
+            <View>
+            <StyledText align='center' color='secondary' fontWeight='bold'>Vamos a empezar</StyledText>
+                    <TouchableOpacity style={styles.button}>
+                        <Text style={styles.buttonText}>Empezar</Text>
+                    </TouchableOpacity>
+            </View>
+            <StyledText align='center' color='secondary' fontWeight='bold'>¿Ya tienes Cuenta?</StyledText>
+                    <TouchableOpacity style={styles.button}>
+                        <Text style={styles.buttonText}>Long in</Text>
+                    </TouchableOpacity>
+            <View>
+
+            </View>
+        </View>
+
+        
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "white",
+        padding: 20,
+        paddingVertical: 20,
+        justifyContent: 'center',
+    },
+
+    button: {
+        backgroundColor: '#789DCA',
+        borderRadius: 40,
+        paddingVertical: 10,
+        paddingHorizontal: 15,
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: '3%',
+        marginBottom: '10%',
+    },
+
+    buttonText: {
+        color: '#fff',
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+});
+
+
+    
